0afe677c15661c5638ac9270531df2c6350bda24
abiu <abiu@appsnt.com>
2018-04-04 14:45:37 +0800

    --

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -6,12 +6,12 @@ export interface IMongoIndex {
 export declare type IMongoIndexes = IMongoIndex[];
 /**
  * {
-        offer:[
-            {key:{offer_id:1,time:-1}},
-            {key:{ttl:1},options:{expireAfterSeconds:3600}}
-        ],
-        user:[{key:{username:1},options:{unique:true}}]
-    }
+ *     offer:[
+ *          {key:{offer_id:1,time:-1}},
+ *           {key:{ttl:1},options:{expireAfterSeconds:3600}}
+ *       ],
+ *       user:[{key:{username:1},options:{unique:true}}]
+ *   }
  */
 export declare type IConfig = Record<string, IMongoIndexes>;
 export interface IConnectOpt {
--- dist/index.js
+++ dist/index.js
@@ -11,43 +11,44 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongodb_1 = require("mongodb");
 function setIndex(connectOpt, config) {
     return __awaiter(this, void 0, void 0, function* () {
-        let conn = yield mongodb_1.MongoClient.connect(connectOpt.uri, connectOpt.options);
-        let db = conn.db(connectOpt.db);
-        for (let col_name in config) {
-            let col = db.collection(col_name);
-            let indexes = config[col_name];
-            for (let i in indexes) {
-                let index = indexes[i];
+        const conn = yield mongodb_1.MongoClient.connect(connectOpt.uri, connectOpt.options);
+        const db = conn.db(connectOpt.db);
+        for (const col_name in config) {
+            const col = db.collection(col_name);
+            const indexes = config[col_name];
+            for (const i in indexes) {
+                const index = indexes[i];
                 try {
                     yield col.createIndex(index.key, index.options);
-                    console.log(col.namespace, "create index ", index.key, index.options || '');
+                    console.log(col.namespace, 'create index ', index.key, index.options || '');
                 }
                 catch (e) {
-                    if (e.code == 85) {
-                        let exit_name = (e.message.match(/name\:\s+.+?name\:\s+"(.+?)",/) || e.message.match(/name\:\s+(.+?)\s/))[1];
+                    if (e.code === 85) {
+                        const exit_name = (e.message.match(/name\:\s+.+?name\:\s+"(.+?)",/) || e.message.match(/name\:\s+(.+?)\s/))[1];
                         yield col.dropIndex(exit_name);
-                        console.log(col.namespace, "drop exit index with conflict ", exit_name);
+                        console.log(col.namespace, 'drop exit index with conflict ', exit_name);
                         yield col.createIndex(index.key, index.options);
-                        console.log(col.namespace, "recreate index ", index.key, index.options);
+                        console.log(col.namespace, 'recreate index ', index.key, index.options);
                     }
-                    else
+                    else {
                         throw e;
+                    }
                 }
             }
-            let exit_indexes = yield col.indexes();
-            let all_index_key = indexes.map(data => JSON.stringify(data.key));
-            let to_drop_indexes = exit_indexes.filter(function (index) {
-                if (all_index_key.indexOf(JSON.stringify(index.key)) == -1 && index.name != '_id_') {
+            const exit_indexes = yield col.indexes();
+            const all_index_key = indexes.map((data) => JSON.stringify(data.key));
+            const to_drop_indexes = exit_indexes.filter(function (index) {
+                if (all_index_key.indexOf(JSON.stringify(index.key)) === -1 && index.name !== '_id_') {
                     return true;
                 }
                 else {
                     return false;
                 }
             });
-            for (let i in to_drop_indexes) {
-                let index = to_drop_indexes[i];
+            for (const i in to_drop_indexes) {
+                const index = to_drop_indexes[i];
                 yield col.dropIndex(index.name);
-                console.log(col.namespace, "drop index useless", index);
+                console.log(col.namespace, 'drop index useless', index);
             }
         }
         yield conn.close();
